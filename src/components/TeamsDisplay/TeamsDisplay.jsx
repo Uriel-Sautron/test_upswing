6c490fcc80f54f4c377ead6492fddc61e432e81b
Uriel <uriel.sautron@gmail.com>
2022-01-03 16:00:37 +0100

    Create algo for checkWin and connect with TeamsDisplay

--- src/components/TeamsDisplay/TeamsDisplay.jsx
+++ src/components/TeamsDisplay/TeamsDisplay.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import { AvatarChipGroup } from '..';
+import { checkWin } from '../../utils';
 import styles from './TeamsDisplay.module.scss';
 
 export const TeamsDisplay = ({ teams }) => {
   const { team1, team2 } = teams;
+  const teamsIsFull = team1.team.length === 5 && team2.team.length === 5;
+  let teamWin = '';
+  if (teamsIsFull) {
+    teamWin = checkWin(team1.team, team2.team);
+  }
   return (
-    (team1.length || team2.length) && (
+    (team1.team.length || team2.team.length) && (
       <section className={styles.teamsDisplayContainer}>
         <div className={styles.teamContainer}>
-          <h2>Team 1</h2>
-          <AvatarChipGroup team={[...team1]} teamName="team1" />
+          <h2>{team1.name}</h2>
+          <AvatarChipGroup team={[...team1.team]} teamName="team1" />
         </div>
+        {teamsIsFull && (
+          <div>
+            <h2>{teamWin === 'Egalité' ? '' : 'Vainqueur'}</h2>
+            <h2>{teamWin}</h2>
+          </div>
+        )}
         <div className={styles.teamContainer}>
-          <h2>Team 2</h2>
-          <AvatarChipGroup team={[...team2]} teamName="team2" />
+          <h2>{team2.name}</h2>
+          <AvatarChipGroup team={[...team2.team]} teamName="team2" />
         </div>
       </section>
     )
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,12 @@
-// checkValue takes a value and returns value if it is truthy or 0 if it is falsy
+// Function checkValue takesas input a value and returns value if it is truthy or 0 if it is falsy
 const checkValue = (value) => {
-  if (value === undefined || value === null) {
+  if (!value) {
     return 0;
   }
-  return value;
+  return parseFloat(value);
 };
 
-// sortPlayers takes as input a array of players and an object { key: string, direction: string } and returns an array of players sorted by the key of the input.
+// Function sortPlayers takes as input an array of players and an object { key: string, direction: string } and returns an array of players sorted by the key of the input.
 export const sortPlayers = (players, sortBy) => {
   const playersSorted = [...players];
   const sortByKey = sortBy;
@@ -22,7 +22,7 @@ export const sortPlayers = (players, sortBy) => {
   return playersSorted;
 };
 
-// searchPlayers takes as input a array of players and a string and returns an array of players that match the search string.
+// Function searchPlayers takes as input an array of players and a string and returns an array of players that match the search string.
 export const searchPlayers = (players, searchValue) => {
   let playersSearch = [...players];
   if (searchValue) {
@@ -34,32 +34,78 @@ export const searchPlayers = (players, searchValue) => {
   return playersSearch;
 };
 
-export const teamStat = (team) => {
+// Function teamStat take as input an array of players and returns an object with the stats of the team.
+export const stats = (team) => {
   const teamStats = {
-    apps: 0 / team.length,
-    mins: 0 / team.length,
-    buts: 0 / team.length,
-    pDecisives: 0 / team.length,
-    jau: 0 / team.length,
-    rou: 0 / team.length,
-    tpM: 0 / team.length,
-    pReu: 0 / team.length,
-    aeriensGagnes: 0 / team.length,
-    hdM: 0 / team.length,
+    apps: 0.0,
+    mins: 0.0,
+    buts: 0.0,
+    pDecisives: 0.0,
+    jau: 0.0,
+    rou: 0.0,
+    tpM: 0.0,
+    pReu: 0.0,
+    aeriensGagnes: 0.0,
+    hdM: 0.0,
   };
 
   team.forEach((player) => {
     teamStats.apps += checkValue(player.Apps);
-    teamStats.mins += checkValue(player.Mins / player.Apps);
-    teamStats.buts += checkValue(player.Buts / player.Apps);
-    teamStats.pDecisives += checkValue(player.PDecisives);
-    teamStats.jau += checkValue(player.Jau / player.Apps);
-    teamStats.rou += checkValue(player.Rou / player.Apps);
-    teamStats.tpM += checkValue(player.TpM);
-    teamStats.pReu += checkValue(player.PReu);
-    teamStats.aeriensGagnes += checkValue(player.AeriensGagnes);
-    teamStats.hdM += checkValue(player.HdM);
+    teamStats.mins += checkValue(player.Mins / player.Apps) / team.length;
+    teamStats.buts += checkValue(player.Buts / player.Apps) / team.length;
+    teamStats.pDecisives += checkValue(player.PDecisives) / team.length;
+    teamStats.jau += checkValue(player.Jau / player.Apps) / team.length;
+    teamStats.rou += checkValue(player.Rou / player.Apps) / team.length;
+    teamStats.tpM += checkValue(player.TpM) / team.length;
+    teamStats.pReu += checkValue(player.PReu) / team.length;
+    teamStats.aeriensGagnes += checkValue(player.AeriensGagnes) / team.length;
+    teamStats.hdM += checkValue(player.HdM) / team.length;
   });
 
   return teamStats;
+};
+
+// Function checWin takes as input two array of players and returns the name of the team that won according to stats.
+export const checkWin = (team1, team2) => {
+  const team1Stats = stats(team1);
+  const team2Stats = stats(team2);
+
+  let team1Score = 0;
+  let team2Score = 0;
+
+  for (const stat in team1Stats) {
+    if (stat !== 'rou' || stat !== 'jau' || stat !== 'hdM') {
+      if (team1Stats[stat] > team2Stats[stat]) {
+        team1Score += 1;
+      } else if (team1Stats[stat] < team2Stats[stat]) {
+        team2Score += 1;
+      }
+    }
+  }
+
+  if (team1Score > team2Score) {
+    return 'Equipe 1';
+  }
+  if (team1Score < team2Score) {
+    return 'Equipe 2';
+  }
+  return 'Egalité';
+};
+
+// Function shuffleArray takes as input a array and returns a shuffled array.
+export const shuffleArray = (array) => {
+  const arrayShuffled = [...array];
+  for (let i = arrayShuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arrayShuffled[i], arrayShuffled[j]] = [arrayShuffled[j], arrayShuffled[i]];
+  }
+  return arrayShuffled;
+};
+
+// Function deletePlayer takes as input an array of players and a player id and returns an array of players without the player that was deleted.
+export const deletePlayer = (players, playerId) => {
+  const playersFiltered = [...players].filter((player) => {
+    return player.id !== playerId;
+  });
+  return playersFiltered;
 };
